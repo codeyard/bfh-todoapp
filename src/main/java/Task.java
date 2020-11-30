fdb3f5c83d5d9b8b947437c96006e323288f69d9
Christophe Leupi <leupc1@bfh.ch>
2020-11-30 19:43:31 +0100

    Task and User Class updated, Klassendiagramm.xml as well

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,93 @@
-public class Task {
+import java.util.Date;
+import java.util.UUID;
+
+public class Task  implements Comparable{
+    private String taskID;
+    private String userID; //todo: needed??
+    private String title;
+    private String category;
+    private Date dueDate;
+    private boolean isImportant = false;
+    private boolean isCompleted = false;
+
+    public Task(String title){
+        this.taskID = UUID.randomUUID().toString();
+        this.title = title;
+    }
+
+    public Task(String title, String category, Date dueDate) {
+        this(title);
+        this.category = category;
+        this.dueDate = dueDate;
+    }
+
+    public Task(String title, String category, Date dueDate, boolean isImportant) {
+        this(title, category, dueDate);
+        this.isImportant = isImportant;
+    }
+
+    public String getTaskID(){
+        return taskID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isImportant() {
+        return isImportant;
+    }
+
+    public void setImportant(boolean important) {
+        isImportant = important;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public boolean isOverdue(){
+        return (dueDate.compareTo(new Date()) < 0) && !isCompleted;
+    }
+
+    @Override
+    public int compareTo(Object task) {
+        if(task instanceof Task){
+            return ((Task) task).getDueDate().compareTo(dueDate);
+        }else{
+            return 0;
+        }
+    }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,108 @@
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
+
 public class User {
+    private String userID;
+    private String userName;
+    private String password;
+    private String firstName; // todo: check if needed
+    private String lastName; // todo: check if needed
+    private String email; // todo: check if needed
+    private Set<Task> tasks = new TreeSet<>();
+
+    public User(String userName, String password) {
+        this.userID =  UUID.randomUUID().toString();
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public boolean authenticate(String userName, String password){
+        return this.userName.equalsIgnoreCase(userName) && this.password.equals(password);
+    }
+
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+
+    public void updateTask(Task task){
+        Iterator<Task> iterator = tasks.iterator();
+        while(iterator.hasNext()) {
+            Task tempTask = iterator.next();
+            if(tempTask.getTaskID().equals(task.getTaskID())){
+                tempTask.setCategory(task.getCategory());
+                tempTask.setCompleted(task.isCompleted());
+                tempTask.setDueDate(task.getDueDate());
+                tempTask.setImportant(task.isImportant());
+                tempTask.setTitle(task.getTitle());
+                break;
+            }
+        }
+    }
+
+    public void deleteTask(Task task){
+        Iterator<Task> iterator = tasks.iterator();
+        while(iterator.hasNext()) {
+            Task tempTask = iterator.next();
+            if(tempTask.getTaskID().equals(task.getTaskID())){
+                iterator.remove();
+                break;
+            }
+        }
+    }
+
+    // todo: check if usermanagment needed? --> addUser()?
 }
