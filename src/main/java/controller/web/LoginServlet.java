4f2b295685e28210cc2bdf01572e64e8473fc263
igorstojanovic91 <i.stojanovic01@gmail.com>
2021-01-24 10:17:25 +0100

    adjusting web.xml

--- src/main/java/controller/web/LoginServlet.java
+++ src/main/java/controller/web/LoginServlet.java
@@ -37,7 +37,6 @@ public class LoginServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        System.out.println("LOGIN SERVLET LOADED");
         response.setContentType("text/html");
         HttpSession session = request.getSession();
         RequestDispatcher view;
